6713bbc11f3e09094cca832dbeb21a5cbdcc4bd5
sweet-axolotl <USBWS00740ME@students.willowsprep.com>
2023-09-27 10:18:30 -0700

    PLEASE WORK OR I WILL THROW COFFEE ON YOUR SERVERS

---
+++ push_to_github.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM Stage all files
+git add -A
+
+REM Commit the changes with the commit message "coffee"
+git commit -m "PLEASE WORK OR I WILL THROW COFFEE ON YOUR SERVERS"
+
+REM Push the changes to the remote repository
+git push origin
