7d525cbb6cdcadc7aa1e97b78c08b5034bae0e4f
sweet-axolotl <USBWS00740ME@students.willowsprep.com>
2023-09-27 10:07:24 -0700

    PLEASE WORK I WILL THROW COFFEE ON YOUR SERVERS IF YOU DON'T

--- PrintName.java
+++ PrintName.java
@@ -1,7 +1,6 @@
-package src.main.java.org.example;
-
 public class PrintName {
-    public static void main(String[] args) {
+    public static void main(String[] arg)
+    {
         String username = System.getProperty("user.name");
         System.out.print("Your name is: " + username + "\n");
     }
