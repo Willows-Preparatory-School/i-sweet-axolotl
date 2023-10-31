316bcb936a2b1eafecf012ba2a4f4aeaca422afb
sweet-axolotl <146225760+sweet-axolotl@users.noreply.github.com>
2023-10-31 08:50:23 -0700

    PLEASE WORK OR I WILL THROW COFFEE ON YOUR SERVERS

--- main.java
+++ main.java
@@ -1,6 +1,24 @@
 public class main {
     public static void main(String[] args)
     {
-        System.out.print("Hello, World!");
+        System.out.print("Hello, World!\n");
+        int[] array = {1, 1, 1 ,1 ,1};
+        System.out.println(fast_power(10,12));
+    }
+
+    public static long fast_power(long base, long power)
+    {
+        long MOD = 1000000007;
+        long result = 1;
+        while(power > 0) {
+
+            if(power % 2 == 1)
+            {
+                result = (result*base) % MOD;
+            }
+            base = (base * base) % MOD;
+            power = power / 2;
+        }
+        return result;
     }
 }
