3d4b0798ab7db3a611445f8a4e7c487b6f242ed6
sweet-axolotl <146225760+sweet-axolotl@users.noreply.github.com>
2023-11-15 09:09:07 -0800

    PLEASE WORK OR I WILL THROW COFFEE ON YOUR SERVERS

--- main.java
+++ main.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args)
     {
         System.out.print("Hello, World!\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Total Amount of nums: ");
+        int totalNumSum = scanner.nextInt();
+        int[] numList = {1,5,6,7};
+        for(int i = 0; i < totalNumSum;i++)
+        {
+            int next = scanner.nextInt();
+            numList[i] = next;
+        }
+        System.out.println(numList);
     }
 
     public static long fast_power(long base, long power)
