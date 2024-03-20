fc085e810b513395f64240c717a24c178e10d83b
sweet-axolotl <146225760+sweet-axolotl@users.noreply.github.com>
2024-03-20 09:17:40 -0700

    cat class update pt. 2 :3

--- c++/cats.cpp
+++ c++/cats.cpp
@@ -7,6 +7,10 @@ class Cat
         std::string getBreed(){return this->breed;}
         int getAge(){return this->age;}
         std::string getName(){return this->name;}
+
+        void setBreed(std::string breed){this->breed = breed;}
+        void setAge(int age){this->age = age;}
+        void setName(std::string name){this->name = name;}
     private:
         std::string breed = "";
         int age = 0;
