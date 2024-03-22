584b8e8531ba68d1ec4db6a50ff74df721683bf6
sweet-axolotl <146225760+sweet-axolotl@users.noreply.github.com>
2024-03-22 11:29:40 -0700

    triangle :3

--- threeD_Test/Main.java
+++ threeD_Test/Main.java
@@ -18,45 +18,6 @@ import org.lwjgl.opengl.*;
 
 public class Main
 {
-    private static final Object[][] cubeVertices = {
-            { -1.0f, 1.0f, 1.0f, 0xff000000 },
-            { 1.0f, 1.0f, 1.0f, 0xff0000ff },
-            { -1.0f, -1.0f, 1.0f, 0xff00ff00 },
-            { 1.0f, -1.0f, 1.0f, 0xff00ffff },
-            { -1.0f, 1.0f, -1.0f, 0xffff0000 },
-            { 1.0f, 1.0f, -1.0f, 0xffff00ff },
-            { -1.0f, -1.0f, -1.0f, 0xffffff00 },
-            { 1.0f, -1.0f, -1.0f, 0xffffffff }
-    };
-
-    private static final int[] cubeIndices = {
-            0, 1, 2, // 0
-            1, 3, 2,
-            4, 6, 5, // 2
-            5, 6, 7,
-            0, 2, 4, // 4
-            4, 2, 6,
-            1, 5, 3, // 6
-            5, 7, 3,
-            0, 4, 1, // 8
-            4, 5, 1,
-            2, 3, 6, // 10
-            6, 3, 7
-    };
-
-    private BGFXVertexLayout layout;
-    private static ByteBuffer vertices;
-    private short vbh;
-    private static ByteBuffer indices;
-    private short ibh;
-    private short program;
-
-    //private Matrix4x3f view = new Matrix4x3f();
-    private FloatBuffer viewBuf;
-    //private Matrix4f proj = new Matrix4f();
-    private FloatBuffer projBuf;
-    //private Matrix4x3f model = new Matrix4x3f();
-    private FloatBuffer modelBuf;
 
     // The window handle
     private static long window;
@@ -125,8 +86,6 @@ public class Main
         glfwMakeContextCurrent(window);
         // Enable v-sync
         glfwSwapInterval(1);
-        // depth test
-        glEnable(GL_DEPTH_TEST);
 
         // Make the window visible
         glfwShowWindow(window);
@@ -140,8 +99,11 @@ public class Main
         // bindings available for use.
         GL.createCapabilities();
 
+        // depth test
+        //glEnable(GL_DEPTH_TEST);
+
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         // TODO: use the other videomode, please.
         GLFWVidMode videomode_render = glfwGetVideoMode(glfwGetPrimaryMonitor());
@@ -150,50 +112,60 @@ public class Main
         // Debug output
         System.out.println("Refresh rate: " + videomode_render.refreshRate() + "hz");
 
+        // other things
+        // Define the vertices of the triangle
+        float[] vertices = {
+                0.0f, 0.5f, 0.0f, // Red vertex
+                0.5f, -0.5f, 0.0f, // Green vertex
+                -0.5f, -0.5f, 0.0f, // Blue vertex
+        };
+
+        // Create a vertex buffer object
+        int vbo = GL30.glGenBuffers();
+        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vbo);
+        GL30.glBufferData(GL30.GL_ARRAY_BUFFER, vertices, GL30.GL_STATIC_DRAW);
+
+        // Set the vertex attribute pointer
+        GL30.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        GL30.glEnableVertexAttribArray(0);
+
+        // Create a shader program
+        String vertexShaderSource =
+                "#version 330 core\n" +
+                        "layout (location = 0) in vec3 position;\n" +
+                        "void main() {\n" +
+                        "    gl_Position = vec4(position, 1.0);\n" +
+                        "}\n";
+        String fragmentShaderSource =
+                "#version 330 core\n" +
+                        "out vec4 fragColor;\n" +
+                        "void main() {\n" +
+                        "    fragColor = vec4(position.x, position.y, 0.0, 1.0);\n" + // Multicolored fragment shader
+                        "}\n";
+        int vertexShader = GL30.glCreateShader(GL30.GL_VERTEX_SHADER);
+        GL30.glShaderSource(vertexShader, vertexShaderSource);
+        GL30.glCompileShader(vertexShader);
+        int fragmentShader = GL30.glCreateShader(GL30.GL_FRAGMENT_SHADER);
+        GL30.glShaderSource(fragmentShader, fragmentShaderSource);
+        GL30.glCompileShader(fragmentShader);
+        int shaderProgram = GL30.glCreateProgram();
+        GL30.glAttachShader(shaderProgram, vertexShader);
+        GL30.glAttachShader(shaderProgram, fragmentShader);
+        GL30.glLinkProgram(shaderProgram);
+
         // Random cube stuff that i forgot how it works, send help
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( !glfwWindowShouldClose(window) ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            // GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+            glClear(GL_COLOR_BUFFER_BIT); // clear the framebuffer
 
             // Render everything in under this line
-            // Rotate the cube
-            glLoadIdentity();
-            glTranslated(x, y, z);
-            glRotatef(pitch, 0.1f, 0.0f, 0.0f);
+            // Use the shader program
+            GL30.glUseProgram(shaderProgram);
 
-            glBegin(GL_QUADS);
-            glColor3f(0,1,1);
-            glVertex3f(-wid/2f, hei/2f, len/2f);
-            glVertex3f(-wid/2f, hei/2f,-len/2f);
-            glVertex3f( wid/2f, hei/2f,-len/2f);
-            glVertex3f( wid/2f, hei/2f, len/2f);
-            glColor3f(1,0,0);
-            glVertex3f(-wid/2f,-hei/2f,-len/2f);
-            glVertex3f( wid/2f,-hei/2f,-len/2f);
-            glVertex3f( wid/2f, hei/2f,-len/2f);
-            glVertex3f(-wid/2f, hei/2f,-len/2f);
-            glColor3f(0,1,0);
-            glVertex3f(-wid/2f,-hei/2f,-len/2f);
-            glVertex3f(-wid/2f,-hei/2f, len/2f);
-            glVertex3f(-wid/2f, hei/2f, len/2f);
-            glVertex3f(-wid/2f, hei/2f,-len/2f);
-            glColor3f(0,0,1);
-            glVertex3f( wid/2f,-hei/2f, len/2f);
-            glVertex3f( wid/2f,-hei/2f,-len/2f);
-            glVertex3f( wid/2f, hei/2f,-len/2f);
-            glVertex3f( wid/2f, hei/2f, len/2f);
-            glColor3f(1,1,0);
-            glVertex3f(-wid/2f,-hei/2f,-len/2f);
-            glVertex3f( wid/2f,-hei/2f,-len/2f);
-            glVertex3f( wid/2f,-hei/2f, len/2f);
-            glVertex3f(-wid/2f,-hei/2f, len/2f);
-            glColor3f(1,0,1);
-            glVertex3f(-wid/2f,-hei/2f, len/2f);
-            glVertex3f( wid/2f,-hei/2f, len/2f);
-            glVertex3f( wid/2f, hei/2f, len/2f);
-            glVertex3f(-wid/2f, hei/2f, len/2f);
-            glEnd();
+            // Draw the triangle
+            GL30.glDrawArrays(GL_TRIANGLES, 0, 3);
             //End of render code.
 
             glfwSwapBuffers(window); // swap the color buffers
