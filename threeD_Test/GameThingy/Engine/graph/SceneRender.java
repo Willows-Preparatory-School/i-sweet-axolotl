d4cce87df755cafaf87d6eb59c3951e427d366c3
spicy_axolotl <84357429+Spicy-Axolotl@users.noreply.github.com>
2024-03-27 19:32:47 -0700

    squaregit add * :3

--- threeD_Test/GameThingy/Engine/graph/SceneRender.java
+++ threeD_Test/GameThingy/Engine/graph/SceneRender.java
@@ -26,7 +26,7 @@ public class SceneRender {
 
         scene.getMeshMap().values().forEach(mesh -> {
                     glBindVertexArray(mesh.getVaoId());
-                    glDrawArrays(GL_TRIANGLES, 0, mesh.getNumVertices());
+                    glDrawElements(GL_TRIANGLES, mesh.getNumVertices(), GL_UNSIGNED_INT, 0);
                 }
         );
 
